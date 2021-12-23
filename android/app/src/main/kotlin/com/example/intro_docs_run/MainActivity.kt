f76102a190bb552d7e7dfddf07c808cce261a2ae
hussamElmaghraby <36246027+hussamElmaghraby@users.noreply.github.com>
2021-12-23 18:33:26 +0200

    initial commit.

---
+++ android/app/src/main/kotlin/com/example/intro_docs_run/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.intro_docs_run
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shopping_list.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:intro_docs_run/product.dart';
+import 'package:intro_docs_run/shopping_list_item.dart';
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, Key? key}) : super(key: key);
+
+  final List<Product> products;
+
+  // The framework calls createState the first time
+  // a widget appears at a given location in the tree.
+  // If the parent rebuilds and uses the same type of
+  // widget (with the same key), the framework re-uses
+  // the State object instead of creating a new State object.
+
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      // When a user changes what's in the cart, you need
+      // to change _shoppingCart inside a setState call to
+      // trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
